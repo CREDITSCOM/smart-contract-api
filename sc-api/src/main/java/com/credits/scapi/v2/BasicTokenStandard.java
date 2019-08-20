5b0daabef0e0da9c97ac6f945b45d29b33a41fea
Igor Goryunov <kongostream@gmail.com>
2019-08-20 17:37:30 +0300

    Revert "refactor: constrain types getTokenBalances"
    This reverts commit 11201d56a8597cbbe73ccee323fcd98090f8eb15.

--- sc-api/src/main/java/com/credits/scapi/v2/BasicTokenStandard.java
+++ sc-api/src/main/java/com/credits/scapi/v2/BasicTokenStandard.java
@@ -2,6 +2,6 @@ package com.credits.scapi.v2;
 
 public interface BasicTokenStandard extends com.credits.scapi.v1.BasicTokenStandard {
 
-    ObservableMap<? extends WalletAddress, ? extends Number> getTokenBalances();
+    ObservableMap getTokenBalances();
 
 }
--- sc-api/src/main/java/com/credits/scapi/v2/WalletAddress.java
+++
@@ -1,7 +0,0 @@
-package com.credits.scapi.v2;
-
-public interface WalletAddress {
-
-    String getBase58Address();
-
-}
